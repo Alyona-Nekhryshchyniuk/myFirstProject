fa79146dd853d64df082962fcacb0a3dfee5296b
Alyona-Nekhryshchyniuk <108234116+Alyona-Nekhryshchyniuk@users.noreply.github.com>
2023-03-06 10:28:57 +0000

    Deploying to gh-pages from @ Alyona-Nekhryshchyniuk/myFirstProject@901c5bf0835560f93be8b0af2335ef85e7a815a9 🚀

---
+++ blog.436e3658.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".header-burger").click((function(e){$(".header-burger,.menu-list").toggleClass("active"),$("body").toggleClass("lock")}))}));
+//# sourceMappingURL=blog.436e3658.js.map
---
+++ blog.f05f9ce0.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".header-burger").click((function(e){$(".header-burger,.menu-list").toggleClass("active"),$("body").toggleClass("lock")}))}));
+//# sourceMappingURL=blog.f05f9ce0.js.map
